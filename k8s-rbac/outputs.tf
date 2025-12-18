813b27b779776229838eaf4bd3210b212678afb6
Benoit Blais <benoit.blais@hashicorp.com>
2025-12-18 11:29:12 -0500

    initial release

--- k8s-rbac/outputs.tf
+++ k8s-rbac/outputs.tf
@@ -1,3 +1,3 @@
 output "oidc_binding_id" {
-  value = kubernetes_cluster_role_binding_v1.oidc_role.id
+  value = length(kubernetes_cluster_role_binding_v1.oidc_role) > 0 ? kubernetes_cluster_role_binding_v1.oidc_role[0].id : null
 }

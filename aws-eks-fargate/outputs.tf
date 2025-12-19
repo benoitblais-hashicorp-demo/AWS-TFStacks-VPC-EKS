ab419cc233e530c96f35d7f42e2837de8506cf30
Benoit Blais <benoit.blais@hashicorp.com>
2025-12-19 09:46:57 -0500

    initial release

--- aws-eks-fargate/outputs.tf
+++ aws-eks-fargate/outputs.tf
@@ -1,11 +1,11 @@
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = length(module.eks) > 0 ? "aws eks --region ${var.region} update-kubeconfig --name ${module.eks[0].cluster_name}" : null
+  value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
 }
 
 output "cluster_name" {
   description = "eks cluster_name"
-  value       = length(module.eks) > 0 ? module.eks[0].cluster_name : null
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
